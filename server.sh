9cfaa84e54d56136e1010e799eb90768aaced620
Hans-Peter Hutter <hans-peter.hutter@zhaw.ch>
2021-05-10 18:12:11 +0200

    Klassenpfad in server.sh mit Anführungszeichen versehen und ";" durch ":" ersetzt.

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED -cp target/swen1_le10_chat-1.0-SNAPSHOT.jar;./libs/tomcat_embed/*;./libs/gson/* ch.zhaw.soe.swen1.le10.chat.api.ChatServerMain $1 $2 $3
+java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED -cp "target/swen1_le10_chat-1.0-SNAPSHOT.jar:./libs/tomcat_embed/*:./libs/gson/*" ch.zhaw.soe.swen1.le10.chat.api.ChatServerMain $1 $2 $3
